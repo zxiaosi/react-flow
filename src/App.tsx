5b236578e097cbe16851150595ae8864cf8ad0ef
zxiaosi <2692159310@qq.com>
2025-04-21 23:34:34 +0800

    feat: 配置 twilwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 function App() {
-  return <></>;
+  return (
+    <div className="h-full w-full">
+      <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+        <div>
+          <div className="text-xl font-bold text-black dark:text-white">
+            ChitChat
+          </div>
+          <p className="text-gray-500 dark:text-gray-400">
+            You have a new message!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
