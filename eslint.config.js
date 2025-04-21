fcccf7bf29cd8202beb3a9bcb7951159d5fd1df3
zxiaosi <2692159310@qq.com>
2025-04-21 23:09:34 +0800

    feat: 删除冗余文件

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import js from '@eslint/js';
-import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
@@ -44,5 +44,5 @@ export default tseslint.config(
       // 可选的 React 规则覆盖
       'react-hooks/exhaustive-deps': 'warn', // 调整为 warn（如需禁用可改为 'off'）
     },
-  }
+  },
 );
