2dda151237ce4f69e8e0834ccd6af6869a2b15e8
zxiaosi <2692159310@qq.com>
2025-04-25 23:42:59 +0800

    feat: 暗转 Zustand 状态管理

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import {
   Background,
   BackgroundVariant,
-  Controls,
-  MiniMap,
   ReactFlow,
   ReactFlowProps,
   ReactFlowProvider,
@@ -11,16 +9,14 @@ import {
 import '@xyflow/react/dist/style.css'; // 引入样式
 import { CustomMenu } from './components';
 
+/**
+ * 老版本谷歌浏览器的兼容性问题
+ * - 遇到 fitView() 方法不生效问题, 可以选择降版本处理, 可以试试v12.4.4版本
+ */
 function App(props: ReactFlowProps) {
   return (
     <div className="h-full w-full">
       <ReactFlow proOptions={{ hideAttribution: true }} {...props}>
-        {/* 控制按钮 */}
-        <Controls />
-
-        {/* 小地图 */}
-        <MiniMap />
-
         {/* 背景 */}
         <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
 
