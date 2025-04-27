d7478ae3108bea2852f0d2d71a3ea2d65873149c
zxiaosi <2692159310@qq.com>
2025-04-27 22:16:47 +0800

    feat: 完善拖拽逻辑

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import {
+  addEdge,
   Background,
   BackgroundVariant,
+  Edge,
+  Node,
   ReactFlow,
   ReactFlowProps,
   ReactFlowProvider,
+  useEdgesState,
+  useNodesState,
+  useReactFlow,
 } from '@xyflow/react';
 
 import { CustomLeftMenu, CustomRightConfig } from '@/components';
@@ -12,18 +18,27 @@ import useProjectConfig from '@/hooks/useProjectConfig';
 import { useShallow } from 'zustand/shallow';
 
 import '@xyflow/react/dist/style.css'; // 引入样式
+import { useCallback } from 'react';
+
+let id = 0;
+const getId = () => `node_${id++}`;
 
 /**
  * 老版本谷歌浏览器的兼容性问题
  * - 遇到 fitView() 方法不生效问题, 可以选择降版本处理, 可以试试v12.4.4版本
  */
 function App(props: ReactFlowProps) {
+  /** react-flow 实例方法 */
+  const { screenToFlowPosition } = useReactFlow();
+
+  /** 项目配置 */
   const { showBg } = useProjectConfig(
     useShallow((state) => ({
       showBg: state.showBg,
     })),
   );
 
+  /** 节点配置 */
   const { drageNodeData, showModal } = useNodeConfig(
     useShallow((state) => ({
       drageNodeData: state.drageNodeData,
@@ -31,9 +46,49 @@ function App(props: ReactFlowProps) {
     })),
   );
 
+  const [nodes, setNodes, onNodesChange] = useNodesState<Node>([]); // 节点
+  const [edges, setEdges, onEdgesChange] = useEdgesState<Edge>([]); // 边
+
+  const onConnect = (params: any) => setEdges((eds) => addEdge(params, eds)); // 连接线
+
+  /** 节点拖拽中事件 */
+  const handleDragOver = useCallback((event: React.DragEvent) => {
+    event.preventDefault(); // 一定要写, 否则onDrop事件不会触发
+  }, []);
+
+  /** 节点拖拽事件 */
+  const handleDrop = useCallback(
+    (event: React.DragEvent) => {
+      event.preventDefault();
+
+      if (!drageNodeData) return;
+
+      const position = screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+      const id = getId();
+      const type = drageNodeData?.name;
+      const newNode = { id, type, position, data: { label: `${type} ${id} ` } };
+
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [drageNodeData],
+  );
+
   return (
     <div className="h-full w-full">
-      <ReactFlow proOptions={{ hideAttribution: true }} {...props}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+        proOptions={{ hideAttribution: true }}
+        {...props}
+      >
         {/* 背景 */}
         {showBg && (
           <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
--- src/components/customLeftMenu/index.tsx
+++ src/components/customLeftMenu/index.tsx
@@ -1,12 +1,21 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import useNodeConfig from '@/hooks/useNodeConfig';
 import useProjectConfig from '@/hooks/useProjectConfig';
 import { Panel, useReactFlow } from '@xyflow/react';
-import { useMemo } from 'react';
 import { useShallow } from 'zustand/shallow';
 
 /** 自定义左侧菜单 */
 const CustomLeftMenu = () => {
+  /** react-flow 实例方法 */
   const { fitView, getEdges, getNodes } = useReactFlow();
 
+  /** 项目配置 */
   const { showBg, onChangeShowBg, theme, onChangeTheme } = useProjectConfig(
     useShallow((state) => ({
       showBg: state.showBg,
@@ -16,43 +25,54 @@ const CustomLeftMenu = () => {
     })),
   );
 
+  /** 节点配置 */
+  const { onDrageNodeData } = useNodeConfig(
+    useShallow((state) => ({ onDrageNodeData: state.onDrageNodeData })),
+  );
+
   /** 菜单数据 */
-  const menuItems = useMemo(() => {
-    return [
-      {
-        name: 'project',
-        label: '项目',
-        children: [
-          {
-            name: 'theme',
-            label: '主题',
-            icon: theme === 'light' ? '&#xe611;' : '&#xe634;',
-          },
-          { name: 'background', label: '背景', icon: '&#xe661;' },
-          { name: 'fitView', label: '铺满', icon: '&#xe6f0;' },
-          { name: 'export', label: '导出', icon: '&#xe60f;' },
-        ],
-      },
-      {
-        name: 'layput',
-        label: '布局',
-        children: [
-          { name: 'horizontal', label: '水平', icon: '&#xe601;' },
-          { name: 'vertical', label: '垂直', icon: '&#xe602;' },
-        ],
-      },
-      {
-        name: 'node',
-        label: '节点',
-        children: [
-          { name: 'default', label: '默认', icon: '&#xe652;' },
-          { name: 'group', label: '组', icon: '&#xe9b3;' },
-          { name: 'square', label: '正方形', icon: '&#xe600;' },
-          { name: 'lozenge', label: '菱形', icon: '&#xe636;' },
-        ],
-      },
-    ];
-  }, [theme]);
+  const menuItems = [
+    {
+      name: 'project',
+      label: '项目',
+      children: [
+        {
+          name: 'theme',
+          label: '主题',
+          icon: theme === 'light' ? '&#xe611;' : '&#xe634;',
+        },
+        { name: 'background', label: '背景', icon: '&#xe661;' },
+        { name: 'fitView', label: '铺满', icon: '&#xe6f0;' },
+        { name: 'export', label: '导出', icon: '&#xe60f;' },
+      ],
+    },
+    {
+      name: 'layput',
+      label: '布局',
+      children: [
+        {
+          name: 'algorithm',
+          type: 'select',
+          options: [
+            { label: 'Dagre', value: 'dagre' },
+            { label: 'Elkjs', value: 'elkjs' },
+          ],
+        },
+        { name: 'horizontal', label: '水平', icon: '&#xe601;' },
+        { name: 'vertical', label: '垂直', icon: '&#xe602;' },
+      ],
+    },
+    {
+      name: 'node',
+      label: '节点',
+      children: [
+        { name: 'default', label: '默认', icon: '&#xe652;' },
+        { name: 'group', label: '组', icon: '&#xe9b3;' },
+        { name: 'square', label: '正方形', icon: '&#xe600;' },
+        { name: 'lozenge', label: '菱形', icon: '&#xe636;' },
+      ],
+    },
+  ];
 
   /** 菜单点击事件 */
   const handleClick = (item) => {
@@ -86,8 +106,9 @@ const CustomLeftMenu = () => {
   };
 
   /** 菜单拖拽开始事件 */
-  const handleDragStart = (e, item) => {
+  const handleDragStart = (e, item, child) => {
     if (item.name !== 'node') return;
+    onDrageNodeData?.(child);
   };
 
   return (
@@ -97,21 +118,39 @@ const CustomLeftMenu = () => {
           <div key={item.name}>
             <div className="mb-2 text-sm font-bold">{item.label}</div>
             <div className="flex flex-wrap gap-2">
-              {item.children.map((child) => (
-                <div
-                  key={child.name}
-                  className={`h-[36px] w-[36px] rounded bg-gray-100 text-gray-600 caret-transparent hover:bg-gray-200 ${item.name === 'node' ? 'cursor-move' : 'cursor-pointer'} `}
-                  draggable={item.name === 'node'}
-                  onClick={() => handleClick(child)}
-                  onDragStart={(e) => handleDragStart(e, child)}
-                >
-                  <span
-                    title={child.label}
-                    className={`iconfont flex h-full w-full items-center justify-center ${!showBg && child.name === 'background' ? 'text-gray-300' : ''}`}
-                    dangerouslySetInnerHTML={{ __html: child.icon }}
-                  ></span>
-                </div>
-              ))}
+              {item.children.map((child) => {
+                return child?.type === 'select' ? (
+                  <Select
+                    key={child.name}
+                    defaultValue={child?.options?.[0]?.value}
+                  >
+                    <SelectTrigger className="w-[86px]">
+                      <SelectValue placeholder="Theme" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {child.options.map((option) => (
+                        <SelectItem key={option.value} value={option.value}>
+                          {option.label}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                ) : (
+                  <div
+                    key={child.name}
+                    className={`h-[36px] w-[36px] rounded bg-gray-100 text-gray-600 caret-transparent hover:bg-gray-200 ${item.name === 'node' ? 'cursor-move' : 'cursor-pointer'} `}
+                    draggable={item.name === 'node'}
+                    onClick={() => handleClick(child)}
+                    onDragStart={(e) => handleDragStart(e, item, child)}
+                  >
+                    <span
+                      title={child.label}
+                      className={`iconfont flex h-full w-full items-center justify-center ${!showBg && child.name === 'background' ? 'text-gray-300' : ''}`}
+                      dangerouslySetInnerHTML={{ __html: child.icon }}
+                    ></span>
+                  </div>
+                );
+              })}
             </div>
           </div>
         ))}
